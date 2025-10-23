942c47cf9615feca2e5346459a578e6df3d5b6d0
ronsell <ronsell42@gmail.com>
2025-10-23 13:45:35 +0800

    Add: Bindig.

--- src/app/components/roles-cmp/roles-cmp.ts
+++ src/app/components/roles-cmp/roles-cmp.ts
@@ -1,17 +1,31 @@
 import { Component, signal, Signal } from '@angular/core';
+import { FormsModule, NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-roles-cmp',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles-cmp.html',
   styleUrl: './roles-cmp.scss'
 })
 export class RolesCmp {
-  projectTitle: string = 'My Angular';
+  projectTitle: string = 'My Angularssss';
   isActive: boolean = false;
   projectVersion: number = 20;
   currentDate: Date = new Date();
-
   varSignal = signal('This is from a signal');
+  selectedValue: string =  '';
+  addedNumber: number = 0;
 
+  // Methods
+  showWelcomeAlert(){
+    alert('This is a normal alert');
+  }
+  alertWithParameter(senderName: string){
+    alert('This alert is from' + senderName);
+   } 
+
+  alertWithType(numberFromHTML: number) : number{
+    this.addedNumber  = numberFromHTML + 10;
+    return this.addedNumber;
+  }
 }
