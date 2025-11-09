9d8e70e742fefb7966701ef6e130c236112a2156
ronsell <ronsell42@gmail.com>
2025-11-09 22:21:08 +0800

    Add: Injected http

--- src/app/components/roles-cmp/roles-cmp.ts
+++ src/app/components/roles-cmp/roles-cmp.ts
@@ -1,4 +1,5 @@
-import { Component, signal, Signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal, Signal } from '@angular/core';
 import { FormsModule, NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
@@ -7,25 +8,16 @@ import { FormsModule, NonNullableFormBuilder } from '@angular/forms';
   templateUrl: './roles-cmp.html',
   styleUrl: './roles-cmp.scss'
 })
-export class RolesCmp {
-  projectTitle: string = 'My Angularssss';
-  isActive: boolean = false;
-  projectVersion: number = 20;
-  currentDate: Date = new Date();
-  varSignal = signal('This is from a signal');
-  selectedValue: string =  '';
-  addedNumber: number = 0;
+export class RolesCmp implements OnInit {
 
-  // Methods
-  showWelcomeAlert(){
-    alert('This is a normal alert');
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getAllRoles();
   }
-  alertWithParameter(senderName: string){
-    alert('This alert is from' + senderName);
-   } 
 
-  alertWithType(numberFromHTML: number) : number{
-    this.addedNumber  = numberFromHTML + 10;
-    return this.addedNumber;
+  getAllRoles(){
+    console.log("Fetching all roles...");
+    this.http.get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles').subscribe((res:any))
   }
 }
