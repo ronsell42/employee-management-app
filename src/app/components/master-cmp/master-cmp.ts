c3d362d61920ab5bdbd024110535c6aa7cea54e6
ronsell <ronsell42@gmail.com>
2025-11-09 12:00:35 +0800

    Add: Adding Pages

--- src/app/components/master-cmp/master-cmp.ts
+++ src/app/components/master-cmp/master-cmp.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { DesignationCmp } from '../designation-cmp/designation-cmp';
+import { RolesCmp } from '../roles-cmp/roles-cmp';
 
 @Component({
   selector: 'app-master-cmp',
-  imports: [],
+  imports: [DesignationCmp, RolesCmp],
   templateUrl: './master-cmp.html',
   styleUrl: './master-cmp.scss'
 })
